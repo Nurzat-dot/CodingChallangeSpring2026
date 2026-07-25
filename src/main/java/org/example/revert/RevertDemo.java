909fa0b9e8828e7d992c8110dacd97431a10ebab
Nurzat <nurzaat1@gmail.com>
2026-07-26 01:21:11 +0700

    This bug has been fixed

--- src/main/java/org/example/revert/RevertDemo.java
+++ src/main/java/org/example/revert/RevertDemo.java
@@ -2,6 +2,7 @@ package org.example.revert;
 
 public class RevertDemo {
     static void main(String[] args) {
-        System.out.println("This Line has a bag!!!");
+        System.out.println("This Line has a bug!!!:");
+        System.out.println("Bug has been fixed");
     }
 }
