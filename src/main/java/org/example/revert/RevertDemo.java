435dd5638bb6672393119416936fd5f38e15280b
Nurzat <nurzaat1@gmail.com>
2026-07-26 01:22:38 +0700

    This bug has been fixed

--- src/main/java/org/example/revert/RevertDemo.java
+++ src/main/java/org/example/revert/RevertDemo.java
@@ -4,5 +4,6 @@ public class RevertDemo {
     static void main(String[] args) {
         System.out.println("This Line has a bug!!!:");
         System.out.println("Bug has been fixed");
+        System.out.println(" The bug has been fixed");
     }
 }
